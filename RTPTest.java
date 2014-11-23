37fbe7cf02f4d1f483b21bed1c3a9e175cdf2edc
Staples <aznchuy@ubuntu.ubuntu-domain>
2014-11-23 16:34:43 -0500

    test case

--- RTPTest.java
+++ RTPTest.java
@@ -6,6 +6,22 @@ public class RTPTest {
 		
 		int x = 0x0000000000000000000000000000001;
 		System.out.println(x);
+
+		int srcPort = 8000;
+		int destPort = 8001;
+		int flags = 8;
+		byte[] receiveWindow = (10000000);
+		byte[] data = "Hello World!".getBytes();
+		RTPDatagram datagram = new RTPDatagram(srcPort, destPort, flags, receiveWindow, data);
+
+		if(datagram.checkChecksum()){
+			System.out.println("checksum works");
+		}
+		else{
+			System.out.println("checksum does not work");
+		}
+
+		
 	}
 
 }
