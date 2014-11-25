5363f94f2280ac3ca95c03f024262c8d1fdbce35
Staples <aznchuy@ubuntu.ubuntu-domain>
2014-11-25 01:47:34 -0500

    compare works with two byte arrays of same sign

--- RTPTest.java
+++ RTPTest.java
@@ -1,62 +1,79 @@
 public class RTPTest {
-	
-	public static void test(){
-		
 
-		int srcPort = 8000;
-		int destPort = 8001;
-		int flags = 8;
-		byte[] receiveWindow = RTPUtil.toBytes(696969);
-		byte[] data = "Hello World!".getBytes();
-		
+	public static void main(String args[]){
+		System.out.println("Hello World");
+		int a = -1;
+		int b = -42875;
+		byte[] x = RTPUtil.toBytes(a);
+		byte[] y = RTPUtil.toBytes(b);
+		System.out.println(RTPUtil.compare(x,y));
+		if(RTPUtil.compare(x, y) == 1 && (a > b) || 
+			RTPUtil.compare(x, y) == 0 && (a == b) || 
+			RTPUtil.compare(x, y) == -1 && (a < b)){
 
-		RTPDatagram datagram1 = new RTPDatagram(srcPort, destPort, flags, receiveWindow, data);
-		datagram1.sequenceNumber = 500;
-		datagram1.ackNumber = 23;
-		datagram1.updateChecksum();
-
-
-		System.out.println(datagram1.toString());
-
-		byte[] datagram1Bytes = datagram1.getByteArray();
-		long checksum1 = datagram1.checksum;
-
-		
-		System.out.println(RTPUtil.printByteArray(datagram1Bytes, 4));
-		
-
-		RTPDatagram datagram2 = new RTPDatagram(datagram1Bytes);	
-		long checksum2 = datagram1.checksum;
-
-		byte[] datagram2Bytes = datagram2.getByteArray();
-		
-		System.out.println(RTPUtil.printByteArray(datagram2Bytes, 4));
-		
-		System.out.println(datagram2.checksum);
-		System.out.println(datagram1.checksum);
-
-		System.out.println(datagram2);
-
-		// System.out.println(datagram1);
-		// System.out.println(datagram2);
-
-		if(datagram2.checkChecksum()){
-			System.out.println("checksum works");
+			System.out.println("PASS");
+		}else{
+			System.out.println("FAIL");
 		}
-		else{
-			System.out.println("checksum does not work");
-		}
-
-		RTPDatagram datagram3 = new RTPDatagram(srcPort, destPort, 4, receiveWindow, data);
-		RTPDatagram datagram4 = new RTPDatagram(srcPort, destPort, 2, receiveWindow, data);
-		RTPDatagram datagram5 = new RTPDatagram(srcPort, destPort, 1, receiveWindow, data);
-		RTPDatagram datagram6 = new RTPDatagram(srcPort, destPort, 3, receiveWindow, data);
-
-
-
-
-
-		
 	}
+	
+	// public static void test(){
+		
+
+	// 	int srcPort = 8000;
+	// 	int destPort = 8001;
+	// 	int flags = 8;
+	// 	byte[] receiveWindow = RTPUtil.toBytes(696969);
+	// 	byte[] data = "Hello World!".getBytes();
+		
+
+	// 	RTPDatagram datagram1 = new RTPDatagram(srcPort, destPort, flags, receiveWindow, data);
+	// 	datagram1.sequenceNumber = 500;
+	// 	datagram1.ackNumber = 23;
+	// 	datagram1.updateChecksum();
+
+
+	// 	System.out.println(datagram1.toString());
+
+	// 	byte[] datagram1Bytes = datagram1.getByteArray();
+	// 	long checksum1 = datagram1.checksum;
+
+		
+	// 	System.out.println(RTPUtil.printByteArray(datagram1Bytes, 4));
+		
+
+	// 	RTPDatagram datagram2 = new RTPDatagram(datagram1Bytes);	
+	// 	long checksum2 = datagram1.checksum;
+
+	// 	byte[] datagram2Bytes = datagram2.getByteArray();
+		
+	// 	System.out.println(RTPUtil.printByteArray(datagram2Bytes, 4));
+		
+	// 	System.out.println(datagram2.checksum);
+	// 	System.out.println(datagram1.checksum);
+
+	// 	System.out.println(datagram2);
+
+	// 	// System.out.println(datagram1);
+	// 	// System.out.println(datagram2);
+
+	// 	if(datagram2.checkChecksum()){
+	// 		System.out.println("checksum works");
+	// 	}
+	// 	else{
+	// 		System.out.println("checksum does not work");
+	// 	}
+
+		// RTPDatagram datagram3 = new RTPDatagram(srcPort, destPort, 4, receiveWindow, data);
+		// RTPDatagram datagram4 = new RTPDatagram(srcPort, destPort, 2, receiveWindow, data);
+		// RTPDatagram datagram5 = new RTPDatagram(srcPort, destPort, 1, receiveWindow, data);
+		// RTPDatagram datagram6 = new RTPDatagram(srcPort, destPort, 3, receiveWindow, data);
+
+
+
+
+
+		
+	// }
 
 }
--- RTPUtil.java
+++ RTPUtil.java
@@ -15,17 +15,17 @@ public class RTPUtil {
 	}
 
 	// Don't think this is needed
-	// public static byte[] toBytes(int i)
-	// {
-	// 	byte[] result = new byte[4];
+	public static byte[] toBytes(int i)
+	{
+		byte[] result = new byte[4];
 
-	// 	result[0] = (byte) (i >> 24);
-	// 	result[1] = (byte) (i >> 16);
-	// 	result[2] = (byte) (i >> 8);
-	// 	result[3] = (byte) (i /*>> 0*/);
+		result[0] = (byte) (i >> 24);
+		result[1] = (byte) (i >> 16);
+		result[2] = (byte) (i >> 8);
+		result[3] = (byte) (i /*>> 0*/);
 
-	// 	return result;
-	// }
+		return result;
+	}
 
 	public static byte[] toShortBytes(int i)
 	{
@@ -58,8 +58,51 @@ public class RTPUtil {
 			return 0;
 		}
 		else{
-			return 1;
+			//x is negative and y is positive
+			if(toInt(x[0]) < 0 && toInt(y[0]) > 0){
+				return 1;
+
+			//x is positive and y is negative	
+			}else if(toInt(x[0]) > 0 && toInt(y[0]) < 0){
+				return -1;	
+
+			//x and y are both negative	
+			}else if(toInt(x[0]) < 0 && toInt(y[0]) < 0){
+				//x has more bytes than y
+				if(x.length > y.length){
+					return -1;
+				//y has more bytes than x	
+				}else if(x.length < y.length){
+					return 1;
+				//x and y have the same number of bytes	
+				}else{
+					for(int i = 0; i< x.length; i++){
+						if(toInt(x[i]) < toInt(y[i])){
+							return -1;
+						}else if(toInt(x[i]) < toInt(y[i])){
+							return 1;
+						}
+					}
+				}
+			//x and y are both positive	
+			}else{
+				if(x.length > y.length){
+					return 1;
+				}else if(x.length < y.length){
+					return -1;
+				}else{
+					for(int i = 0; i < x.length; i++){
+						if(toInt(x[i]) > toInt(y[i])){
+							return 1;
+						}else if(toInt(x[i]) < toInt(y[i])){
+							return -1;
+						}
+					}
+				}
+			}
 		}
+
+		return 0;
 		// Return 0 if they're the same
 		// Return 1 if x is bigger
 		// Return -1 if y is bigger
@@ -72,9 +115,7 @@ public class RTPUtil {
 		// TIM DO THIS
 		// Watch out for "signed" vs "unsigned"
 		// x and y should both be unsigned
-<<<<<<< HEAD
 		// You can use my printing utilities to help you
-=======
 		int size;
 		if(x.length > y.length){
 			size = x.length;
@@ -86,7 +127,7 @@ public class RTPUtil {
 		for(int i = 0; i < size; i++){
 			result[i] = (byte)(x[i] + y[i]);
 		}
->>>>>>> e96fa7ba87038ba499adc835ef2af21a306798f2
+		//e96fa7ba87038ba499adc835ef2af21a306798f2
 
 
 		// return sum of x and y in new byte array
